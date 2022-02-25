8857e6d115dfe3012fdff9c2569f7107900977ed
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-25 20:03:30 +0200

    Create problem_2.java

---
+++ task_1/problem_2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.IOException;
+
+public class task-1_2 {
+
+   
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        double max, min;
+        long n = input.nextLong();
+        max = (n/ Math.log(N));
+        min = (n / Math.log(N)) * 1.25506;
+        System.out.println(max + " " + min);
+
+    }
+
+}
