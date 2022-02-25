0d0de5e3b0e048a19a9a1b529840f5a1c0c602cc
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-02-25 21:27:40 +0200

    Update problem_1.java

--- task_1/problem_1.java
+++ task_1/problem_1.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+
+public class Tssk-1{
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x= input.nextInt();
+       if (x <= 800) {
+    System.out.println("1");
+  } else if (x > 800 && dis <= 1400) {
+   System.out.println("2");
+  } else if (x > 1400 && dis <= 2000) {
+   System.out.println("3");
+  }
+    
+    }
+}
