747e58a84da7614c1678a7827f3b38a94e9ee2db
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-03-10 02:18:18 +0200

    Create problem_19.java

---
+++ task-3/problem_19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int n =input.nextInt();
+        String a[] = new String[1];
+        int c=0;
+        int s =0;
+        a[0] =s.next();
+        for(int i =1;i<a[0].length();i++){
+            if(a[0].charAt(star)==a[0].charAt(i)){
+                c++;
+                s++;
+            }
+            else{
+                s++;
+            }
+        }
+         System.out.println(c);
+    }
+    }
